05ea047509b19837e072fd21ef206c0b0a375749
Sid Masih <sid.masih@gmail.com>
2016-09-12 16:37:47 -0700

    working up to exceptions

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,6 @@
 class HangpersonGame
 
+  attr_accessor :word, :guesses, :wrong_guesses, :mutableword
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -10,6 +11,24 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @mutableword = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+
+  def guess(letter)
+    letter = letter.downcase
+    if (@guesses.include?(letter) or @wrong_guesses.include?(letter))
+      
+      return false
+    elsif (@mutableword.include?(letter) or @mutableword.include?(letter)) 
+      @guesses = @guesses + letter
+       @mutableword = @mutableword.tr(letter, '')
+      return true
+    else
+      @wrong_guesses = @wrong_guesses + letter
+      return true
+    end
   end
 
   def self.get_random_word
