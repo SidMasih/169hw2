92c6400579071808f80472610fb2fa50fd47cfe6
Sid Masih <sid.masih@gmail.com>
2016-09-12 18:41:37 -0700

    website works?

--- app.rb
+++ app.rb
@@ -39,7 +39,14 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    begin
+    status = @game.guess(letter)
+    if !status
+      flash[:message] =  "You have already used that letter."
+    end
+    rescue ArgumentError 
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
@@ -50,16 +57,25 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :win
+      redirect '/show'
+    end
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :lose
+      redirect '/show'
+    end
     erb :lose # You may change/remove this line
   end
   
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -14,22 +14,56 @@ class HangpersonGame
     @mutableword = word
     @guesses = ""
     @wrong_guesses = ""
+    @guesses_left = 7
   end
 
   def guess(letter)
+    if(letter == nil or letter.length == 0 or letter[/[a-zA-Z]+/]  != letter )
+      raise ArgumentError, "bad args"
+    end
+
+
     letter = letter.downcase
     if (@guesses.include?(letter) or @wrong_guesses.include?(letter))
       
       return false
     elsif (@mutableword.include?(letter) or @mutableword.include?(letter)) 
       @guesses = @guesses + letter
-       @mutableword = @mutableword.tr(letter, '')
+      @mutableword = @mutableword.tr(letter, '')
       return true
     else
+      @guesses_left = @guesses_left - 1
       @wrong_guesses = @wrong_guesses + letter
       return true
     end
   end
+
+  def word_with_guesses
+    myword = @word
+    wordarray = @word.split("")
+
+    for i in wordarray
+      if(!@guesses.include?(i))
+        myword = myword.tr(i,"-")
+      end
+    end
+
+    return myword
+    
+  end
+
+  def check_win_or_lose
+    if(@guesses_left <= 0)
+      return :lose
+    elsif (@mutableword.length ==0)
+      return :win
+    else 
+      return :play
+    end
+      
+
+  end
+
 
   def self.get_random_word
     require 'uri'
